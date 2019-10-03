8357d6aaa7b37f0f091ff47162b4c2729037a7cc
Wei Ning <macgnw@gmail.com>
2019-10-03 11:28:17 +1000

    openfile

---
+++ scripts/filesystem/openfile_std.ml
@@ -0,0 +1,35 @@
+#!/usr/bin/env ocaml
+
+(* 
+source
+https://ocaml.org/learn/tutorials/file_manipulation.html
+
+ocaml unix shows how to perform file-open using unix specific functions 
+(syscalls) but its example is too outdated to compile
+*)
+
+open Printf
+
+let file = "/var/tmp/sut/prt.txt"
+let message = "Hello!"
+  
+let () =
+  (* Write message to file *)
+  let oc = open_out file in    (* create or truncate file, return channel *)
+  fprintf oc "%s\n" message;   (* write something *)   
+  close_out oc;                (* flush and close the channel *)
+  
+  (* Read file and display the first line *)
+  let ic = open_in file in
+  try 
+    let line = input_line ic in  (* read line from in_channel and discard \n *)
+    print_endline line;          (* write the result to stdout *)
+    flush stdout;                (* write on the underlying device now *)
+    close_in ic                  (* close the input channel *) 
+  
+  with e ->                      (* some unexpected exception occurs *)
+    close_in_noerr ic;           (* emergency closing *)
+    raise e                      (* exit with error: files are closed but
+                                    channels are not flushed *)
+  
+  (* normal exit: all channels are flushed and closed *)
