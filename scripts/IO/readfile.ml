357798513cff4c50b30b0d2b8b0d58eccc3c9953
Wei Ning <macgnw@gmail.com>
2019-10-06 14:01:04 +1100

    read all lines

--- scripts/IO/readfile.ml
+++ scripts/IO/readfile.ml
@@ -1,14 +1,35 @@
 #!/usr/bin/env ocaml
 
-let processLine line =
-  print_endline line;
+#load "str.cma";;
+
+let process line =
+  (* 
+  https://caml.inria.fr/pub/docs/manual-ocaml/libref/Str.html
+  global_replace regexp templ s 
+  
+  see also:
+  https://stackoverflow.com/questions/37513046/using-str-module-in-ocaml-top-level?rq=1
+  *)
+  let line' = Str.global_replace (Str.regexp "[a-zA-Z]+") "000" line in
+  print_endline line';
+  ;;
+
+let transform line = 
+  Str.global_replace (Str.regexp "[a-zA-Z]+") "000" line
+  ;;
+
+let output line oc = 
+  output_string oc line;
+  output_string oc "\n";
   ;;
 
 let demoReadFile () =
+  let oc = open_out "/var/tmp/sut/doom" in
   let ic = open_in "/etc/passwd" in
   try while true do
-    let line = input_line ic in processLine line
-  done with End_of_file -> close_in ic
+    let line = input_line ic in output (transform line) oc
+  done with End_of_file -> close_in ic;
+  close_out oc;
   ;;
 
 let runDemos () = 
