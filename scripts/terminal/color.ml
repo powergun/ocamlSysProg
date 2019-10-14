e24a2ab4dfe3a719c16f1b6d7e7768d6997b80ec
Wei Ning <macgnw@gmail.com>
2019-10-15 08:44:51 +1100

    terminal color example

---
+++ scripts/terminal/color.ml
@@ -0,0 +1,34 @@
+#!/usr/bin/env coretop
+
+open Core;;
+open Stdio;;
+
+type basic_color = Black
+                 | Red
+                 | Green
+                 | Yellow
+                 | Blue
+                 | Magenta
+                 | Cyan
+                 | White
+
+let basic_color_to_int = function
+  | Black -> 0 
+  | Red -> 1 
+  | Green -> 2 
+  | Yellow -> 3
+  | Blue -> 4 
+  | Magenta -> 5 
+  | Cyan -> 6 
+  | White -> 7
+
+let color_by_number number text =
+  Printf.sprintf "\027[38;5;%dm%s\027[0m" number text
+  ;;
+
+let runDemos () =
+  let templ = color_by_number (basic_color_to_int Blue) "cow" in
+  printf "there is a %s\n" templ;
+  ;;
+
+runDemos();;
